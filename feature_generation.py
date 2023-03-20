7b5d781a23fbc128c614be8edacaa3d372218aed
Bryce Richards <brycerichards89@gmail.com>
2023-03-20 08:08:45 -0400

    Refactored feature_generation.py.

--- feature_generation.py
+++ feature_generation.py
@@ -4,10 +4,10 @@ from sklearn.utils import shuffle
 
 import re
 
+from copy import deepcopy
 
 import imp
 import bullets
-imp.reload(bullets)
 from blurbs import BlurbFeatures
 from bullets import BulletFeatures
 
@@ -50,45 +50,36 @@ class CrossTermComputer:
         return ret_df
 
 
-class ZipcodeEncoder:
-    def __init__(self):
+class DummyEncoder:
+    def __init__(self, col_name, min_count = 10):
+        self.col_name = col_name
+        self.min_count = min_count
+        self.training_dummies_df = None
         self.encoder = None
-        self.training_dummy_zips_df = None
         self.col_names = None
-    
-    # Actually this is unneeded since we already extracted zip codes :-/
-    def extract_zip(self, address):
-        zip_re = r'.*\, SC (2[0-9]{4})$'
-        m = re.match(zip_re, address)
-        return m.group(1)
 
-    def _convert_results_to_df(self, dummy_zips_np):
+    # TODO: make this a general dummy_encoding_to_df utility method.
+    def _convert_results_to_df(self, dummies_np):
         col_names = self.encoder.get_feature_names_out()
-        col_names = [name.replace('x0', 'zip') for name in col_names]
+        col_names = [name.replace('x0', self.col_name) for name in col_names]
         infrequent_col_names = [name for name in col_names if name.find('infrequent') != -1]
-        zip_df = pd.DataFrame(dummy_zips_np, columns=col_names)
-        print("zip df shape before dropping infrequent cols ", zip_df.shape)
-        zip_df.drop(columns=infrequent_col_names, inplace=True)
-        print("zip df shape after dropping infrequent cols ", zip_df.shape)
-        return zip_df
+        dummies_df = pd.DataFrame(dummies_np, columns=col_names)
+        dummies_df.drop(columns=infrequent_col_names, inplace=True)
+        return dummies_df
         
-    def generate_training_zipcode_dummy_vars(self, df, min_zipcode_count=10):
+    def generate_training_dummy_vars(self, df, min_zipcode_count=10):
         self.encoder = OneHotEncoder(handle_unknown="ignore", sparse_output = False, min_frequency=min_zipcode_count)
-        Dummy_Zips = self.encoder.fit_transform(df[['zip']])  # note that here we do fit_transform
-
-
-        #Dummy_Zips = self.encoder.fit_transform(df.zip.to_numpy().reshape(n, 1))  # note that here we do fit_transform
-        zips_df = self._convert_results_to_df(Dummy_Zips)
-        self.training_dummy_zips_df = zips_df
-        return zips_df
-
-    
-    def generate_testing_zipcode_dummy_vars(self, df):
+        dummies_np = self.encoder.fit_transform(df[[self.col_name]])  # note that here we do fit_transform
+        dummies_df = self._convert_results_to_df(dummies_np)
+        self.training_dummies_df = dummies_df
+        return dummies_df
+   
+    def generate_testing_dummy_vars(self, df):
         n = df.shape[0]
-        Dummy_Zips = self.encoder.transform(df.zip.to_numpy().reshape(n, 1))  # note that here we just transform - encoder was fit on training data
-        zips_df = self._convert_results_to_df(Dummy_Zips)
-        print("zips_df.shape ", zips_df.shape)
-        return zips_df
+        dummies_np = self.encoder.transform(df[[self.col_name]])  # note that here we just transform - encoder was fit on training data
+        dummies_df = self._convert_results_to_df(dummies_np)
+        return dummies_df
+
 
 
 # splits apt_df and addr_df into training, cross-validation, and testing subsets.
@@ -118,75 +109,36 @@ def split_data(apt_df, addr_df):
     print('addr_df shape after: ', addr_df.shape)
 
 
+    # define indices for training and testing data
     num_addrs = addr_df.shape[0]
     addr_df = shuffle(addr_df).reset_index(drop=True)
-    num_train = int(0.65 * num_addrs)
-    num_val = int(0.2 * num_addrs)
-    num_test = num_addrs - (num_train + num_val)
+    num_train = int(0.70 * num_addrs)
+    num_test = num_addrs - num_train
     train_indices = pd.Series([True for i in range(num_train)] + [False for i in range(num_addrs - num_train)])
-    val_indices = pd.Series([False for i in range(num_train)] + [True for i in range(num_val)] + [False for i in range(num_test)])
-    test_indices = ~(train_indices | val_indices)
-    addr_df_train = addr_df.loc[train_indices].reset_index(drop=True)
-    addr_df_validation = addr_df.loc[val_indices].reset_index(drop=True)
-    addr_df_test = addr_df.loc[test_indices].reset_index(drop=True)
-
-    # Fuck all this for now. Let's just split the addresses 65:20:15 and go for it.
-    # TODO: fix this code to split on number of units, to ensure balanced split.
-    # 
-    # redefine this to account for missing data
-    # apt_df['units_in_building'] = 
-    # # add the unit count to the addr_df, so that we can split the buildings up but count how many units are put into each dataset.
-    # addrs_and_unit = apt_df[['address', 'units_in_building']].drop_duplicates()
-    # addr_with_unit_count = addr_df.merge(addrs_and_unit, on='address').sort_values(by=['units_in_building'])
-    # print('address_with_unit_count.shape ', addr_with_unit_count.shape)
-    # num_units = apt_df.shape[0]
-    # min_training_units = int(0.7 * num_units)
-    # min_validation_units = int(0.2 * num_units)
+    test_indices = pd.Series([False for i in range(num_train)] + [True for i in range(num_test)])
     
-    # print('min training units, min val units, sum', min_training_units, min_validation_units, min_training_units + min_validation_units)
-    # print('num total units: ', num_units)
-
-    # addr_with_unit_count = shuffle(addr_with_unit_count)
-    # addr_with_unit_count.reset_index(inplace=True, drop = True)
-    # addr_with_unit_count['unit_sum'] = addr_with_unit_count.units_in_building.cumsum()
-
-    # training_indices = addr_with_unit_count.unit_sum <= min_training_units
-    # val_indices = (addr_with_unit_count.unit_sum > min_training_units) & (addr_with_unit_count.unit_sum <= min_training_units + min_validation_units)
-    # test_indices = ~(training_indices | val_indices)
-    # print("indices shape: ", val_indices.shape)
-
-    # print("num training indices:", training_indices.sum())
-    # print("num val indices", val_indices.sum())
-    # print("num test indices", test_indices.sum())
-
-    # training_indices.to_csv('/tmp/train_indices.csv')
-    # val_indices.to_csv('/tmp/val_indices.csv')
-
-    # addr_df_train = addr_with_unit_count.loc[addr_with_unit_count.unit_sum <= min_training_units]
-    # addr_df_validation = addr_with_unit_count.loc[(addr_with_unit_count.unit_sum > min_training_units) & (addr_with_unit_count.unit_sum <= min_training_units + min_validation_units)].reset_index(drop=True)
-    # # addr_df_test = addr_with_unit_count.loc[addr_with_unit_count.unit_sum > (min_training_units + min_validation_units)].reset_index(drop=True)
-    # addr_df_test = addr_with_unit_count.loc[test_indices].reset_index(drop=True)
-    # # addr_df_train.drop(columns=['units_in_building', 'unit_sum'], inplace = True)
-    # # addr_df_validation.drop(columns=['units_in_building', 'unit_sum'], inplace = True)
-    # # addr_df_test.drop(columns=['units_in_building', 'unit_sum'], inplace = True)
-
-
+    # create training and testing dataframes
+    addr_df_train = addr_df.loc[train_indices].reset_index(drop=True)
+    addr_df_test = addr_df.loc[test_indices].reset_index(drop=True)
     apt_df_train = pd.merge(apt_df, addr_df_train[['address']], on='address')
-    apt_df_validation = pd.merge(apt_df, addr_df_validation[['address']], on='address')
     apt_df_test = pd.merge(apt_df, addr_df_test[['address']], on='address')
 
-    return ((apt_df_train, addr_df_train), (apt_df_validation, addr_df_validation), (apt_df_test, addr_df_test))
-
-
-
+    return ((apt_df_train, addr_df_train), (apt_df_test, addr_df_test))
 
 
 class FeatureGenerator:
-    def __init__(self):
+    def __init__(self, min_bullet_count = 20, num_svd_components = 15, min_zipcode_dummy_count = 10):
         self.cross_term_computer = None
         self.blurb_features = BlurbFeatures()
-        self.zip_encoder = ZipcodeEncoder()
-        self.bullet_features = BulletFeatures(min_bullet_count = 20, num_svd_components = 20)
+        
+        # Dictionary mapping from column name to the DummyEncoder object for that column.
+        # This will actually let us create new encoders on the fly in get_training_features().
+        self.dummy_encoders = dict()
+        self.dummy_encoders["zip"] = DummyEncoder(col_name="zip", min_count = min_zipcode_dummy_count)
+        self.dummy_encoders["property_type"] = DummyEncoder(col_name="property_type", min_count = 3)
+        self.property_type_encoder = OneHotEncoder(handle_unknown="ignore", sparse_output = False, min_frequency=10)
+        self.bullet_features = BulletFeatures(min_bullet_count=min_bullet_count,
+                                              num_svd_components=num_svd_components)
     
     def _merge_svd_feats_and_apt_df(self, svd_df, X, addr_df):
         # we have to concat these, instead of merge them, because scikitlearn just returns a numpy array.
@@ -200,21 +152,25 @@ class FeatureGenerator:
         latlng_list = re.sub(r'[\(\)]', '', latlng).split(',')
         [lat, lng] = map(lambda s : float(s.strip()), latlng_list)
         return (lat, lng)
+        return (X, y)
 
-    def _add_lat_long_features(self, apt_df, addr_df):
-        addrs_and_latlng = addr_df[['address', 'latlng']]
-        addrs_and_latlng.drop_duplicates(inplace=True)
-
-        addrs_and_latlng['lat'] = addrs_and_latlng.latlng.map(lambda s : self._extract_lat_and_lng(s)[0])
-        addrs_and_latlng['lng'] = addrs_and_latlng.latlng.map(lambda s : self._extract_lat_and_lng(s)[1])
-        addrs_and_latlng['lat^2'] = addrs_and_latlng.lat ** 2
-        addrs_and_latlng['lng^2'] = addrs_and_latlng.lng ** 2
-        addrs_and_latlng['lat * lng'] = addrs_and_latlng.lat * addrs_and_latlng.lng
-        addrs_and_latlng.drop(columns=['latlng'], inplace=True)
-        X = apt_df.merge(addrs_and_latlng, on='address')
-        return X
+    def _add_lat_long_features(self, addr_feats):
+        addr_feats['lat'] = addr_feats.latlng.map(lambda s : self._extract_lat_and_lng(s)[0])
+        addr_feats['lng'] = addr_feats.latlng.map(lambda s : self._extract_lat_and_lng(s)[1])
+        addr_feats['lat^2'] = addr_feats.lat ** 2
+        addr_feats['lng^2'] = addr_feats.lng ** 2
+        addr_feats['lat * lng'] = addr_feats.lat * addr_feats.lng
+        return addr_feats
     
-    def _add_property_type(self, X, addr_df):
+    def _convert_ptype_to_df(self, ptype_dummies, encoder):
+        col_names = encoder.get_feature_names_out()
+        col_names = [name.replace('x0', 'ptype') for name in col_names]
+        infrequent_col_names = [name for name in col_names if name.find('infrequent') != -1]
+        ptype_df = pd.DataFrame(ptype_dummies, columns=col_names)
+        ptype_df.drop(columns=infrequent_col_names, inplace=True)
+        return ptype_df
+    
+    def _add_property_type(self, addr_feats):
         def type_from_title(title):
             m = re.match(r'.* ([A-Za-z]+) for Rent.*', title)
             if m is None:
@@ -223,12 +179,8 @@ class FeatureGenerator:
             if type == 'houses':
                 type = 'house'
             return type
-        addr_df['property_type'] = addr_df.title.map(type_from_title)
-        addr_and_type = addr_df[['property_type', 'address']]
-        addr_and_type = pd.concat((addr_and_type, pd.get_dummies(addr_and_type.property_type)), axis=1)
-        X_with_type = X.merge(addr_and_type, on='address')
-        return X_with_type
-
+        addr_feats['property_type'] = addr_feats.title.map(type_from_title)
+        return addr_feats
 
     
     def _merge_bullet_svd_and_apt_df(self, bullet_svd_df, apt_df, addr_df):
@@ -237,67 +189,86 @@ class FeatureGenerator:
         assert(addrs_and_bullet_svd_feats.shape[0] == bullet_svd_df.shape[0])
         X_with_bullets = apt_df.merge(addrs_and_bullet_svd_feats, on='address')
         return X_with_bullets
+    
+    def _add_bullet_feats(self, addr_df, addr_feats, training = True):
+        bullet_svd_df = None
+        if training:
+            bullet_svd_df = self.bullet_features.get_training_svd_df(addr_df)
+        else:
+            bullet_svd_df = self.bullet_features.get_testing_svd_df(addr_df)
+        addr_feats_new = addr_feats.join(bullet_svd_df)
+        assert(addr_feats.shape == addr_feats.shape)
+        return addr_feats_new
+    
+    def _add_blurb_feats(self, addr_feats, training = True):
+        svd_df = None
+        if training:
+            self.blurb_features.compute_training_tfidf_matrix(addr_feats.blurb)
+            tfidf_mat = self.blurb_features.get_training_tfidf_matrix()
+            svd_df = self.blurb_features.compute_training_svd_df(training_tfidf_matrix=tfidf_mat)
+        else:
+            svd_df = self.blurb_features.compute_testing_svd_df_from_blurbs(addr_feats.blurb)
+        addr_feats_new = pd.concat((addr_feats, svd_df), axis=1)
+        return addr_feats_new
+        
+
+    def _add_furnished(self, addr_feats):
+        def is_furnished(bullets):
+            return int("()Furnished." in bullets)
+        addr_feats['furnished'] = addr_feats.bullets.map(is_furnished)
+        return addr_feats
+    
+    def _add_dummies(self, addr_feats, col_name, training=True):
+        dummies_df = None
+        if training:
+            dummies_df = self.dummy_encoders[col_name].generate_training_dummy_vars(addr_feats)
+        else:
+            dummies_df = self.dummy_encoders[col_name].generate_testing_dummy_vars(addr_feats)
+        addr_feats_new = pd.concat((addr_feats, dummies_df), axis = 1)
+        return addr_feats_new
+    
+    def _merge_addr_and_apt_features(self, addr_features, apt_features):
+        merged_features = apt_features.merge(addr_features, on='address')
+        return merged_features
+
+    def _convert_col_to_dummies(self, addr_features, col_name, training=True):
+        """ stores encoder in self.col_to_encoders[col_name] """
+
+    def _get_features(self, apt_df, addr_df, training):
+        apt_features = apt_df.sort_values(by=['address']).reset_index(drop=True)
+        addr_features = deepcopy(addr_df)
+        addr_features = self._add_lat_long_features(addr_features)
+        addr_features = self._add_blurb_feats(addr_features, training = training)
+        addr_features = self._add_bullet_feats(addr_df=addr_df, addr_feats=addr_features, training=training)
+        addr_features = self._add_furnished(addr_features)
+        addr_features = self._add_dummies(addr_features, col_name="zip", training=training)
+        # To add the property type dummy vars, first we need to create a "property_type" column
+        # Perhaps this should be done in a separate step.
+        addr_features = self._add_property_type(addr_features)
+        addr_features = self._add_dummies(addr_features, col_name="property_type", training=training)
+
+        X = self._merge_addr_and_apt_features(addr_features, apt_features)
+
+        # Generate the cross-terms
+        columns = list(addr_features.columns)
+        zipcode_dummy_columns = [col for col in columns if 'zip_' in col]
+        basic_columns = ['beds', 'baths', 'sq_ft', 'units_in_building']
+        column_pairs = [('sq_ft', 'sq_ft'), ('beds', 'beds'), ('baths', 'baths')]
+        self.cross_term_computer = CrossTermComputer(first_columns=zipcode_dummy_columns, second_columns=basic_columns, column_pairs=column_pairs)
+        X_cross_terms = self.cross_term_computer.compute_cross_terms(X)
+        X = pd.concat((X, X_cross_terms), axis=1)
+
+        y = X[['price']]
+        X = X.drop(columns=['price'])
+        return (X, y)
 
     def get_training_features(self, apt_df_train, addr_df_train):
-        X0 = apt_df_train.sort_values(by=['address']).reset_index(drop=True)
-        print("X0 shape before add lat lng ", X0.shape)
-        X0 = self._add_lat_long_features(X0, addr_df_train)
-        print("X0 shape after add lat lng ", X0.shape)
-
-        # 1) generate the zip code dummy variables
-        X_zips = self.zip_encoder.generate_training_zipcode_dummy_vars(X0)
-        X1 = pd.concat((X0, X_zips), axis=1)
-
-        # 2) Generate the features from the description blurbs
-        # fit and generate the SVD features from the blurbs
-        self.blurb_features.compute_training_tfidf_matrix(addr_df_train.blurb)
-        tfidf_mat = self.blurb_features.get_training_tfidf_matrix()
-        svd_df = self.blurb_features.compute_training_svd_df(training_tfidf_matrix=tfidf_mat)
-        X2 = self._merge_svd_feats_and_apt_df(svd_df, X1, addr_df_train)
-
-        # 3) Generate the cross-terms
-        first_columns = list(X_zips.columns)
-        second_columns = ['beds', 'baths', 'sq_ft', 'units_in_building']
-        column_pairs = [('sq_ft', 'sq_ft'), ('beds', 'beds'), ('baths', 'baths')]
-        self.cross_term_computer = CrossTermComputer(first_columns=first_columns, second_columns=second_columns, column_pairs=column_pairs)
-        X_cross_terms = self.cross_term_computer.compute_cross_terms(X2)
-        X3 = pd.concat((X2, X_cross_terms), axis=1)
-
-        # 4) Generate bullet features (dummy vars and latent concepts)
-        bullet_svd_df = self.bullet_features.get_training_svd_df(addr_df_train)
-        X4 = self._merge_bullet_svd_and_apt_df(bullet_svd_df, X3, addr_df_train)
-
-        # 5) Other features
-        X5 = self._add_property_type(X4, addr_df_train)
-
-        y = X5[['price']]
-        X5 = X5.drop(columns=['price'])
-        return (X5, y)
+        (X, y) = self._get_features(apt_df_train, addr_df_train, training = True)
+        return (X, y)
     
     def get_testing_features(self, apt_df_test, addr_df_test):
-        X0 = apt_df_test.sort_values(by=['address']).reset_index(drop=True)
-
-        X0 = self._add_lat_long_features(X0, addr_df_test)
-
-        X_zips = self.zip_encoder.generate_testing_zipcode_dummy_vars(X0)
-        X1 = pd.concat((X0, X_zips), axis = 1)
-
-        svd_df = self.blurb_features.compute_testing_svd_df_from_blurbs(addr_df_test.blurb)
-        X2 = self._merge_svd_feats_and_apt_df(svd_df, X1, addr_df_test)
-
-        X_cross_terms = self.cross_term_computer.compute_cross_terms(X2)
-        X3 = pd.concat((X2, X_cross_terms), axis=1)
-
-        bullet_svd_df = self.bullet_features.get_testing_svd_df(addr_df_test)
-        X4 = self._merge_bullet_svd_and_apt_df(bullet_svd_df, X3, addr_df_test)
-        
-        X5 = self._add_property_type(X4, addr_df_test)
-
-        y = X5[['price']]
-        X5 = X5.drop(columns=['price'])
-
-        return (X5, y)
-
+        (X, y) = self._get_features(apt_df_test, addr_df_test, training = False)
+        return (X, y)
 
 
 def select_column_names(all_column_names,
@@ -306,7 +277,8 @@ def select_column_names(all_column_names,
                         zip_feats = False,
                         blurb_feats = False,
                         bullet_feats = False,
-                        building_type = False,
+                        property_type = False,
+                        furnished = False,
                         cross_term_res = []):
     returned_cols = ['beds', 'baths', 'sq_ft']  # we will always include these features
     if units_in_building:
@@ -336,9 +308,14 @@ def select_column_names(all_column_names,
             m = re.match(bullets_re, col)
             if m:
                 returned_cols.append(col)
-    if building_type:
-        type_cols = ['apartment', 'condo', 'house', 'townhouse', 'undefined']
+    if property_type:
+        # property_types = ['apartment', 'condo', 'house', 'townhouse', 'undefined']
+        property_types = ['house', 'townhouse', 'undefined']
+        type_cols = ['property_type_' + p_type for p_type in property_types]
         returned_cols.extend(type_cols)
+    
+    if furnished:
+        returned_cols.append('furnished')
     
     if cross_term_res is None:
         return returned_cols
